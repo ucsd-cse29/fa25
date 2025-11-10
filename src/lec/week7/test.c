76b8f346dc0cf7f6636552c414fdde6dd62a243a
Joe Politz <joe.politz@gmail.com>
2025-11-10 12:33:38 -0800

    .

--- src/lec/week7/test.c
+++ src/lec/week7/test.c
@@ -2,13 +2,9 @@
 #include <stdio.h>
 
 int main() {
-  char* a = malloc(10);
-  char* b = malloc(37);
-  int* c = malloc(7 * sizeof(int));
-  printf("After allocating a, b, c:\n");
-  print_heap();
-  free(b);
-  int* d = malloc(5 * sizeof(int));
-  printf("\nAfter freeing b and allocating d:\n");
-  print_heap();
+  char* a = mymalloc(40);
+  char* b = mymalloc(24);
+  char* c = mymalloc(80);
+
+  printf("%p %p %p\n", a, b, c);
 }
